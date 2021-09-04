9b0817ebecef53d844782533985dccd86d19a03c
Thiago mendes <thiagomendes0601@gmail.com>
2021-09-04 18:04:42 -0300

    Atividades

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -23,6 +23,10 @@ export class TemaService {
     return this.http.get<Tema>(`https://blogpessoalth.herokuapp.com/tema/${id}`, this.token)
   }
 
+  getByNomeTema(nome: string): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`https://blogpessoalth.herokuapp.com/tema/nome${nome}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blogpessoalth.herokuapp.com/tema', tema, this.token)
   }
